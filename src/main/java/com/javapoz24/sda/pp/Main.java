9abb38e77ef64afc08f9ebbf118c53d3c4a90eac
Korejap <Korejap@users.noreply.github.com>
2020-07-04 14:08:59 +0200

    AdviceClient print

--- src/main/java/com/javapoz24/sda/pp/Main.java
+++ src/main/java/com/javapoz24/sda/pp/Main.java
@@ -3,8 +3,9 @@ package com.javapoz24.sda.pp;
 public class Main {
     public static void main(String[] args) {
 
-new AdviceClient().fetchAdvice();
-
+AdviceClient adviceClient = new AdviceClient();
+Slip slip = adviceClient.fetchAdvice();
+        System.out.println(slip.getAdvice());
 
     }
 }
